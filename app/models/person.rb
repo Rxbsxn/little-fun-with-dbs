44308e25d29bd38b900357650316a5899c948ed1
Robert Harężlak <harezlak.robert7@gmail.com>
2017-03-27 14:13:52 +0200

    get to know how before_save working

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,10 @@
 class Person < ApplicationRecord
+  before_save :add_job
   validates :first_name, :last_name, presence: true
   validates :last_name, uniqueness: { case_sensitive: false }
   validates :age, numericality: { greater_than: 18}
+
+  def add_job
+    self.job = "Programmer"
+  end
 end
