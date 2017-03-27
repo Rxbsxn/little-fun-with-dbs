dcb9da0111b788a908015c9de4c6aa7617d3234a
Robert Harężlak <harezlak.robert7@gmail.com>
2017-03-27 14:10:41 +0200

    model validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,5 @@
 class Person < ApplicationRecord
+  validates :first_name, :last_name, presence: true
+  validates :last_name, uniqueness: { case_sensitive: false }
+  validates :age, numericality: { greater_than: 18}
 end
